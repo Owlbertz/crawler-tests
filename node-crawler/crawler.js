789f1cd84282457669c7f6f4395f3102172128ab
Marius Olbertz <marius.olbertz@gmail.com>
2017-06-26 19:24:32 +0200

    Fixed endless crawling caused by wrong URL normalization.

--- node-crawler/crawler.js
+++ node-crawler/crawler.js
@@ -5,6 +5,15 @@ let crawledUrls = [];
 let queuedUrls = [baseUrl];
 let titleBase = ' | Foundation for Sites 6 Docs';
 let startDate = new Date();
+
+let normalizeUrl = function(currentUrl) {
+    currentUrl = currentUrl.replace(/\/\//g, '/'); // Normalize double slash
+    currentUrl = currentUrl.replace(':/', '://'); // Fix broken protocol
+    currentUrl = currentUrl.substr(0, currentUrl.indexOf('#') > -1 ? currentUrl.indexOf('#') : currentUrl.length); // Remove hash
+    currentUrl = currentUrl.replace(/\/$/, '');// Normalize trailing slash
+    return currentUrl;
+};
+
 let c = new Crawler({
     maxConnections: 5,
     // rateLimit: 500,
@@ -17,9 +26,8 @@ let c = new Crawler({
             // $ is Cheerio by default
             //a lean implementation of core jQuery designed specifically for the server
             let currentUrl = baseUrl + '/' + res.request.uri.path;
-            currentUrl = currentUrl.replace(/\/\//g, '/'), // Normalize double slash
-            currentUrl = currentUrl.replace(/\/$/, ''), // Normalize trailing slash
-            // console.log($('title').text().replace(titleBase, ''), '\t', currentUrl);
+            currentUrl = normalizeUrl(currentUrl);
+            // console.log(crawledUrls.length.toString(), $('title').text().replace(titleBase, ''), '\t', currentUrl);
             queuedUrls.splice(queuedUrls.indexOf(currentUrl), 1);
             crawledUrls.push(currentUrl);
 
@@ -32,7 +40,9 @@ let c = new Crawler({
             let urls = [];
             $links.each(function() {
                 let url = baseUrl + '/' + $(this).attr('href');
+                url = normalizeUrl(url);
                 if (urls.indexOf(url) === -1 && crawledUrls.indexOf(url) === -1 && queuedUrls.indexOf(url) === -1) {
+                    // console.log('Adding', url);
                     urls.push(url);
                 }
             });
@@ -48,7 +58,7 @@ let c = new Crawler({
 // Queue just one URL, with default callback
 c.queue(baseUrl);
 
-c.on('drain',function(){
+c.on('drain',function() {
     let endDate = new Date();
     let duration = endDate - startDate;
     console.log(`Finished crawl, found ${crawledUrls.length} pages, it tool ${duration} ms.`);
