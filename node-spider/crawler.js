f823f6021ea37e86f8fd1f382ca5399322aef53c
Marius Olbertz <marius.olbertz@gmail.com>
2017-06-26 19:24:52 +0200

    Finalized first implementation of node-spider crawler.

--- node-spider/crawler.js
+++ node-spider/crawler.js
@@ -2,15 +2,17 @@ var Spider = require('node-spider');
 
 let baseUrl = process.argv[2]; //'http://localhost:3000';
 console.log(baseUrl);
-var crawledUrls = [];
+let crawledUrls = [];
+let titleBase = ' | Foundation for Sites 6 Docs';
+let startDate = new Date();
 
 var spider = new Spider({
 	// How many requests can be run in parallel
 	concurrent: 5,
 	// How long to wait after each request
-	delay: 1000 * 10,
+	delay: 1000 * 0.5,
 	// A stream to where internal logs are sent, optional
-	logs: process.stderr,
+	// logs: process.stderr,
 	// Re-visit visited URLs, false by default
 	allowDuplicates: false,
 	// If `true` all queued handlers will be try-catch'd, errors go to `error` callback
@@ -23,10 +25,13 @@ var spider = new Spider({
 	keepAlive: false,
 	// Called when there's an error, throw will be used if none is provided
 	error: function(err, url) {
+		console.log(err, url);
 	},
 	// Called when there are no more requests
 	done: function() {
-    	console.log(`Finished crawl, found ${crawledUrls.length} pages, it tool ${duration} ms.`);
+		let endDate = new Date();
+		let duration = endDate - startDate;
+		console.log(`Finished crawl, found ${crawledUrls.length} pages, it tool ${duration} ms.`);
 	},
 
 	//- All options are passed to `request` module, for example:
@@ -34,11 +39,20 @@ var spider = new Spider({
 	encoding: 'utf8'
 });
 
+
+let normalizeUrl = function(currentUrl) {
+    currentUrl = currentUrl.replace(/\/\//g, '/'); // Normalize double slash
+    currentUrl = currentUrl.replace(':/', '://'); // Fix broken protocol
+    currentUrl = currentUrl.substr(0, currentUrl.indexOf('#') > -1 ? currentUrl.indexOf('#') : currentUrl.length); // Remove hash
+    currentUrl = currentUrl.replace(/\/$/, '');// Normalize trailing slash
+    return currentUrl;
+};
+
 var handleRequest = function(doc) {
 	// new page crawled
-	console.log(doc.res); // response object
-	console.log(doc.url); // page url
-	crawledUrls.push(doc.url);
+	let currentUrl = normalizeUrl(doc.url);
+	console.log(crawledUrls.length.toString(), doc.$('title').text().replace(titleBase, ''), '\t', currentUrl);
+	crawledUrls.push(currentUrl);
 	if (crawledUrls.length % 50 === 0) {
 		console.log(`Crawled ${crawledUrls.length} sites`);
 	}
@@ -46,9 +60,12 @@ var handleRequest = function(doc) {
 	// uses cheerio, check its docs for more info
 	doc.$('a').each(function(i, elem) {
 		// do stuff with element
-		var href = doc.$(elem).attr('href').split('#')[0];
-		var url = doc.resolve(href);
-		// crawl more
+		var href = doc.$(elem).attr('href');
+		if (!href || (href.indexOf('http') === 0)) return;
+
+		let url = baseUrl + '/' + href;
+		url = normalizeUrl(url);
+
 		spider.queue(url, handleRequest);
 	});
 };
